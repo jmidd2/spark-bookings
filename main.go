3dcd224e80425347affb9512bc366a750046e17b
Jon M <jonpmiddleton@gmail.com>
2025-08-07 20:45:46 -0700

    Basic layout with embedded templates

--- main.go
+++ main.go
@@ -1,34 +1,52 @@
 package main
 
 import (
+	"embed"
+	"fmt"
 	"html/template"
 	"net/http"
+	"time"
 )
 
-type Todo struct {
+type Event struct {
 	Title string
 	Done  bool
+	Date  time.Time
 }
 
-type TodoPageData struct {
+type EventPageData struct {
 	PageTitle string
-	Todos     []Todo
+	Events    []Event
 }
+
+var (
+	//go:embed templates/*
+	templatesFS embed.FS
+)
 
 func main() {
-	tmpl := template.Must(template.ParseFiles("index.html"))
+	staticFS := http.FileServer(http.Dir("assets/"))
+	http.Handle("/static/", http.StripPrefix("/static/", staticFS))
+
+	test, err := template.ParseFS(templatesFS, "templates/*.html")
+	if err != nil {
+		panic(err)
+	}
+	tmpl := template.Must(test, err)
 
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		data := TodoPageData{PageTitle: "Todos", Todos: []Todo{
-			{Title: "Buy milk", Done: false},
-			{Title: "Buy eggs", Done: true},
+		fmt.Printf("Handling request: %s\n", r.URL.Path)
+		data := EventPageData{PageTitle: "Todos", Events: []Event{
+			{Title: "Some Meeting", Done: false},
+			{Title: "All the people", Done: true},
 		}}
 		if err := tmpl.Execute(w, data); err != nil {
+			println(err.Error())
 			panic(err)
 		}
 	})
 
-	err := http.ListenAndServe(":8080", nil)
+	err = http.ListenAndServe(":8080", nil)
 	if err != nil {
 		return
 	}
